b68d7eb76c6a8bbf3997fb4fe287a9b54ed40d04
Zuberi <ajan.zuberi.ext@yunextraffic.com>
2025-10-21 10:55:28 +0200

    feat: first Release that works

--- SETUP/install.ps1
+++ SETUP/install.ps1
@@ -7,26 +7,32 @@ if ($backupChoice -eq "" -or $backupChoice -match '^[Yy]$') {
 }
 
 # Delete old profile
-Write-Host "Deleting current profile..."
+Write-Host "Deleting current profile..." -ForegroundColor Red
 Remove-Item $PROFILE -ErrorAction SilentlyContinue
 
 # Copy new profile
-Write-Host "Copying new profile..."
-Copy-Item .\Microsoft.PowerShell_profile.ps1 $PROFILE
-Write-Host "Profile copied to $PROFILE"
-
-# Install requirements
-Write-Host "Installing required modules..."
-. .\requirements.ps1
-Write-Host "Requirements installed."
+Write-Host "Copying new profile..." -ForegroundColor Cyan
+Copy-Item ..\Microsoft.PowerShell_profile.ps1 $PROFILE
+Write-Host "Profile copied to $PROFILE" -ForegroundColor Green
 
 # Copy scripts
-Write-Host "Copying scripts..."
-Copy-Item .\custom_scripts\* (Split-Path $PROFILE -Parent)\custom_scripts -Recurse -Force
-Copy-Item .\Scripts\* (Split-Path $PROFILE -Parent)\Scripts -Recurse -Force
-Write-Host "Scripts copied."
+Write-Host "Copying scripts..." -ForegroundColor Cyan
+# Create script folders
+New-Item -ItemType Directory -Path (Join-Path (Split-Path $PROFILE -Parent) "custom_scripts") -Force | Out-Null
+New-Item -ItemType Directory -Path (Join-Path (Split-Path $PROFILE -Parent) "Scripts") -Force | Out-Null
+
+Copy-Item ..\custom_scripts\ -Destination (Split-Path $PROFILE -Parent) -Recurse -Force
+Copy-Item ..\Scripts\ -Destination (Split-Path $PROFILE -Parent) -Recurse -Force
+Write-Host "Scripts copied." -ForegroundColor Green
 
 # Copy Oh-My-Posh themes
-Copy-Item .\themes\* $env:POSH_THEMES_PATH -Recurse -Force
+Write-Host "Copying themes..." -ForegroundColor Cyan
+Copy-Item ..\themes\* $env:POSH_THEMES_PATH -Recurse -Force
+Write-Host "Themes copied." -ForegroundColor Green
+
+# Install requirements
+Write-Host "Installing required modules..." -ForegroundColor Cyan
+. .\requirements.ps1
+Write-Host "Requirements installed." -ForegroundColor Green
 
 Write-Host "Installation complete! Please restart your PowerShell session to apply changes."
